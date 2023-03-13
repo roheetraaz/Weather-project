dfe0d6eca41031ce3815f42476457deb1cc9672f
roheetraaz <roheetraaz@gmail.com>
2023-03-13 21:58:04 +0530

    Push to github

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Weather-project
-React project that uses dual api for searching cities and predicting weather for a week .
-
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Search from './components/Search/Search';
+
+function App() {
+
+ const handleOnSearchChange=(searchData)=>{
+  console.log(searchData);
+ }
+
+
+  return (
+    <div className="container">
+     <Search onSearchChange={handleOnSearchChange}/>
+    </div>
+  );
+}
+
+export default App;
